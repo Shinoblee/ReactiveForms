2ea811dfc5836e2b0524cb5df762664add3edafd
Lee Simon <shinoblee@gmail.com>
2020-05-07 10:38:00 -0400

    Angular Reactive Forms example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,94 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, Validators, FormGroup, FormArray } from "@angular/forms";
+import { forbiddenNameValidator } from "./shared/username.validator";
+import { passwordValidator } from "./shared/password.validator";
+import { RegistrationService } from "./registration.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  title = 'reactive-forms';
+export class AppComponent implements OnInit {
+  constructor(
+    private fb: FormBuilder,
+    private registrationService: RegistrationService
+  ) {}
+
+  registrationForm: FormGroup;
+
+  ngOnInit() {
+    this.registrationForm = this.fb.group(
+      {
+        username: [
+          "",
+          [
+            Validators.required,
+            Validators.minLength(3),
+            forbiddenNameValidator(/password/),
+          ],
+        ],
+        email: [""],
+        promotional: [false],
+        password: [""],
+        confirmPassword: [""],
+        address: this.fb.group({
+          city: [""],
+          state: [""],
+          postalCode: [""],
+        }),
+        alternateEmails: this.fb.array([]),
+      },
+      { validator: passwordValidator }
+    );
+
+    this.registrationForm
+      .get("promotional")
+      .valueChanges.subscribe((checkedValue) => {
+        const email = this.registrationForm.get("email");
+        if (checkedValue) {
+          email.setValidators(Validators.required);
+        } else {
+          email.clearValidators();
+        }
+        email.updateValueAndValidity();
+      });
+  }
+
+  loadApiData() {
+    this.registrationForm.patchValue({
+      username: "Bruce",
+      password: "test",
+      confirmPassword: "test",
+      // address: {
+      //   city: "City",
+      //   state: "Columbus",
+      //   postalCode: "43228",
+      // },
+    });
+  }
+
+  addAlternateEmail() {
+    this.alternateEmails.push(this.fb.control(""));
+  }
+
+  onSubmit() {
+    console.log(this.registrationForm.value);
+    this.registrationService.register(this.registrationForm.value).subscribe(
+      (response) => console.log("Success", response),
+      (error) => console.error("Error", error)
+    );
+  }
+
+  get username() {
+    return this.registrationForm.get("username");
+  }
+
+  get email() {
+    return this.registrationForm.get("email");
+  }
+
+  get alternateEmails() {
+    return this.registrationForm.get("alternateEmails") as FormArray;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, ReactiveFormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
